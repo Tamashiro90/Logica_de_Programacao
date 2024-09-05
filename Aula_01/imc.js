1fd376496bebd9ee1d81d34a339cf2d394109088
Tamashiro90 <leandro.tamashiro90@gmail.com>
2024-09-04 21:33:19 -0300

    Add files via upload

---
+++ Aula_01/imc.js
@@ -0,0 +1,19 @@
+var name_1;
+var name_2;
+
+name_1='João';
+console.log(name_1);
+name_2='Maria';
+console.log(name_2);
+
+//calcular o imc de uma pessoa . IMC-pesa/(altura)²
+
+var peso;
+var altura;
+
+
+peso=90;
+altura=1.83;
+
+imc = peso/altura**2
+console.log(imc.toFixed(2)); //comando .toFixed reduz ou aumenta casas decimais
---
+++ Aula_01/nomecompleto.js
@@ -0,0 +1,11 @@
+/*Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira varável chamada "nomeCompleto" e imprima ao usuário*/
+
+var nome;
+var sobrenome;
+
+nome="Leandro";
+sobrenome="Tamashiro";
+
+nomeCompleto=(nome+" "+sobrenome)
+
+console.log(nomeCompleto)
