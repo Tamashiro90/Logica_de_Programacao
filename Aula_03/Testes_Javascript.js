c895c7b30c6b41190d088b1f488d97636eca80ee
Tamashiro90 <leandro.tamashiro90@gmail.com>
2024-09-06 21:26:17 -0300

    Add files via upload

---
+++ Aula_03/Testes_Javascript.js
@@ -0,0 +1,22 @@
+let numerointeiro = parseInt("3.9");
+console.log(numerointeiro)
+
+let numero = parseFloat("3.9");
+console.log(numero)
+
+let vinte = (prompt("Digite 20 e pressione Enter:"));
+console.log(vinte+1 );
+
+let nome = "Maria Eduarda";
+let idade = 20;
+console.log(nome,"tem",idade,"anos")
+
+let nome_02 = "Geovana";
+let idade_02 = 21;
+alert(nome_02+" tem "+idade_02+" anos")
+
+let anoAtual = 2021;
+let nascimento = 1990;
+let nome_03 = "José"
+alert(`${nome_03} nasceu em ${nascimento}.
+Se estamos em ${anoAtual}, ele certamente tem ${anoAtual - nascimento} anos.`);
