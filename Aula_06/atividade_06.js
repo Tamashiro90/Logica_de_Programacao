1481b0ca377188464b814c0e008c9d827a7659c3
Tamashiro90 <leandro.tamashiro90@gmail.com>
2024-09-16 21:27:44 -0300

    Add files via upload

---
+++ Aula_06/atividade_06.js
@@ -0,0 +1,27 @@
+//Função principal
+function caixaDeMercado(){
+    let totalGeral=0;
+    let numProduto=parseInt(prompt("Quantos produtos você está comprando"));
+    let recibo="";
+
+    //Estrutura de repetição para capturar informação de cada produto
+    for (let i=0; i<numProduto; i++) {
+        let nomeProduto = prompt(`Nome do produto ${i + 1}:`);
+        let quantidade = parseInt(prompt(`Quantidade de ${nomeProduto}:`));
+        let valorUnitario = parseFloat(prompt(`Valor unitário de ${nomeProduto}`));
+
+        //Calcula o valor total do produto
+        let valorTotalProduto = quantidade*valorUnitario;
+        totalGeral += valorTotalProduto;
+
+        //Adiciona as informações ao recibo
+        recibo += `${nomeProduto}: ${quantidade} x R$ ${valorUnitario.toFixed(2)} = R$ ${valorTotalProduto.toFixed(2)}\n`
+    }
+    //Exibe o recibo e o total geral
+    recibo += `\nTotal geral: R$ ${totalGeral.toFixed(2)}`;
+    alert(recibo);
+}
+//Chama a função
+caixaDeMercado();
+
+
